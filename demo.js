fcd01e96378c35bc3e1bfd0f023220f328fdad8b
Ryan Albright <rpa56776@uga.edu>
2023-10-07 23:11:02 -0400

    edits to js

--- demo.js
+++ demo.js
@@ -1,1 +1,12 @@
-console.log("hello hi")
+//console.log("hello hi")
+/*function suqareit(a) {
+    return a*a;
+}*/
+
+//const squareit = function(a){
+//    return a*a;
+//}
+//const sum = (x, y) => {return x + y; };
+//const squareit = (a) => {return a*a};
+const squareit = num => num*num;
+console.log(squareit(4));
